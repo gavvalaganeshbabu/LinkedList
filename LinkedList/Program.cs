c431ff80f73dc1cccb8a79e1f0dcf0692ebfea7c
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 15:33:44 +0530

    Add project files.

---
+++ LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList L=new LinkedList();
+            L.Add(56);
+            L.Add(30);   
+            L.Add(70);
+            L.Display();
+        }
+    }
+}
