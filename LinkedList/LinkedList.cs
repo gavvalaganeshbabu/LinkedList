307b47e62129207551133aed0ef2862820f75ab4
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-17 10:00:51 +0530

    DeleteElement method added

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -25,6 +25,7 @@ namespace LinkedList
             }
             Console.WriteLine("{0} Element inserted into LinkedList", Node.data);
         }
+<<<<<<< HEAD
         public void Display()
         {
             Node temp = this.head;
@@ -75,6 +76,48 @@ namespace LinkedList
                 }
             }
             return head;
+=======
+        public void DeleteElement(int position, int data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            Node temp = head;
+            if (position == 0)
+            {
+                head = temp.next;
+                return;
+            }
+            for (int i = 1; temp != null && i < position - 1; i++)
+            {
+                temp = temp.next;
+            }
+            if (temp == null || temp.next == null)
+            {
+                return;
+            }
+            Node next1 = temp.next.next;
+            temp.next = next1;
+        }
+        public void Display()
+        {
+            Console.WriteLine("After Deletion :");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+>>>>>>> UC9
         }
     }
 }
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -12,11 +12,16 @@ namespace LinkedList
             LinkedList L = new LinkedList();
             L.Add(56);
             L.Add(30);
+            L.Add(40);
             L.Add(70);
+<<<<<<< HEAD
             Console.WriteLine("Before Inserting ");
             L.Display();
             L.InsertElement(3,40);
             Console.WriteLine("After Inserting ");
+=======
+            L.DeleteElement(3,40);
+>>>>>>> UC9
             L.Display();
         }
     }
